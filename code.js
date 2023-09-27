b779889765136a158481d734ec487be032c137c8
Ttbias2 <tobiasvalentini73@gmail.com>
2023-09-27 12:50:02 -0300

    version final

--- code.js
+++ code.js
@@ -6,13 +6,9 @@ const numPoke = document.getElementById("numPoke");
 const stats = document.querySelectorAll(".stats");
 const contenedorPoke = document.getElementById("contenedorPoke");
 
-// Agregar un evento al campo de entrada
 ittPoke.addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
-        // Evitar el comportamiento predeterminado del Enter (enviar el formulario)
         event.preventDefault();
-        
-        // Llamar a la función para realizar la búsqueda
         cambiarNombre();
     }
 });
